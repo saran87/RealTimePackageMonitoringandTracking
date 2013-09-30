d938f7b7d130350d03b8ab0b3a9d487004663579
nxj3261 <nanditagsee@gmail.com>
2013-09-30 18:59:24 -0400

    Revert "Added SensorList changes"
    This reverts commit 45881e97fea2608767c467a082ed342ea473cf05.

--- code/Android/SensorInfo/SensorInfo/src/com/rtpmt/packtrack/SensorInfo.java
+++ code/Android/SensorInfo/SensorInfo/src/com/rtpmt/packtrack/SensorInfo.java
@@ -1,6 +1,5 @@
 package com.rtpmt.packtrack;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -15,43 +14,27 @@ import java.util.Set;
 public class SensorInfo{
 
 	Long MacID;
-	int ShortID;
-	int SerialID;
-	int PackageID;
-	int TruckID;
-	ArrayList<ServiceInfo> services;
-	
-	
-	
-	// add the Service List here
-	
-	//int tempThreshold;
-	//int tempFreq;
+	int tempThreshold;
+	int tempFreq;
 	//int humidityThreshold;
 	//int humidityFreq;
 	//int vibrationThreshold;
 	//int vibrationFreq;
 	
 	
-	public SensorInfo(Long _MacID, int _shortID, int _serialID, int _packageID, int _truckID){
+	public SensorInfo(Long _MacID,int _tempThreshold,int _tempFreq){
 		
 		MacID = _MacID; 
-		ShortID = _shortID;
-		SerialID = _serialID;
-		PackageID = _packageID;
-		TruckID = _truckID;
-			
+		tempThreshold =_tempThreshold;
+		tempFreq = _tempFreq;
+		
+		
 	}
 
 	
-    public SensorInfo  getSensorInfo(){
+  public SensorInfo  getSensorInfo(){
 		
 		return(this);
 		
-	}
-    
-    public void addService(ServiceInfo _ServiceInfo){
-    	if(services == null)  new ArrayList<ServiceInfo>() ;
-    	services.add(_ServiceInfo);
 	}
 }
--- code/Android/SensorInfo/SensorInfo/src/com/rtpmt/packtrack/SensorList.java
+++ code/Android/SensorInfo/SensorInfo/src/com/rtpmt/packtrack/SensorList.java
@@ -5,70 +5,95 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-
+/**
+ * 
+ * @author Apurv Kulkarni
+ *
+ * @param <M> MAC ID .
+ * @param <S> Short ID
+ */
 
 public class SensorList<M,S>
 {
-	//HashMap<M,S> macIds;
-	HashMap<S, SensorInfo> SensorMap;
+	HashMap<M,S> macIds;
+	HashMap<S,M> shortIds;
 	
 	public SensorList()
 	{
-		//macIds = new HashMap<M,S>(); 
-		SensorMap = new HashMap<S,SensorInfo>();
+		macIds = new HashMap<M,S>(); 
+		shortIds = new HashMap<S,M>();
 	}
 
 	public void clear() {	 
-		//macIds.clear();
-		SensorMap.clear();		
+		macIds.clear();
+		shortIds.clear();		
 	}
 
-	public boolean containsKey(S shortId) {
-		
-		 if(SensorMap.containsKey(shortId))
+	public boolean containsKey(Object macOrShort) {
+		if(macIds.containsKey(macOrShort))
+			return true;
+		else if(shortIds.containsKey(macOrShort))
 			return true;
 		else
 			return false;
 	}
 	
+	public boolean constainsMac(M macId){
+		if(macIds.containsKey(macId))
+			return true;
+		else
+			return false;
+	}
 	
-	
-
-	public boolean containsValue(Object SensorInfo) {
-		 if(SensorMap.containsKey(SensorInfo))
+	public boolean constainsShort(S shortId){
+		if(shortIds.containsKey(shortId))
 			return true;
 		else
 			return false;
 	}
 
-	public SensorInfo get(S key) {
-		if(SensorMap.containsKey(key))
-			return SensorMap.get(key);
+	public boolean containsValue(Object macOrShort) {
+		if(macIds.containsValue(macOrShort))
+			return true;
+		else if(shortIds.containsKey(macOrShort))
+			return true;
+		else
+			return false;
+	}
+
+	public M get(S key) {
+		if(shortIds.containsKey(key))
+			return shortIds.get(key);
 		else
 			return null;
 	}
 
-	
+	public boolean isEmpty() {
+		return macIds.isEmpty();
+	}
+
 	public Set<S> ShortIdSet() {
-		return SensorMap.keySet();
+		return shortIds.keySet();
 	}
 	
+	public Set<M> MacIdSet() {
+		return macIds.keySet();
+	}
 
-
-	public S put( S shortId, SensorInfo sensorInfo){
-		
-		SensorMap.put(shortId, sensorInfo);
+	public S put(M macId, S shortId) {
+		macIds.put(macId, shortId);
+		shortIds.put(shortId, macId);
 		return shortId;
 	}
 
-	public S removeShortID(S shortId) {
-		
-		SensorMap.remove(shortId);
+	public S removeMacId(M macId) {
+		S shortId = macIds.remove(macId);
+		shortIds.remove(shortId);
 		return shortId;
 	}
 
 	public int size() {
-		return SensorMap.size();
+		return macIds.size();
 	}
 
 }
