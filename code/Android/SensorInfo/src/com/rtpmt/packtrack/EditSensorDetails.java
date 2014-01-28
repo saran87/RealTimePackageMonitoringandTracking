155380c1a11ca343c666dba6e241fe7b396cc5d4
Pratima <prit@pratima.(none)>
2014-01-28 18:24:28 -0500

    adding minor changes-- display settings for services as disabled when switch is off

--- code/Android/SensorInfo/src/com/rtpmt/packtrack/EditSensorDetails.java
+++ code/Android/SensorInfo/src/com/rtpmt/packtrack/EditSensorDetails.java
@@ -15,8 +15,9 @@ import com.example.sensorinfo.R;
 public class EditSensorDetails extends Activity {
 
 	public final static String EXTRA_MESSAGE1 = "com.example.sensordata.MESSAGE";
-	public final static String SENSOR_ID = "short_id";
+	public final static String SENSOR_ID = "sensor_id";
 	private String selectedSensorId;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -27,41 +28,70 @@ public class EditSensorDetails extends Activity {
 		int sensorPosition = intent.getIntExtra(StartActivity.POSITION, 0);
 		String packageId = listOfSensors.getSensors(sensorPosition)
 				.getPackageId();
-		selectedSensorId = listOfSensors.getSensors(sensorPosition).getSensorId();
-				
+		selectedSensorId = listOfSensors.getSensors(sensorPosition)
+				.getSensorId();
+
 		EditText editText1 = (EditText) findViewById(R.id.edit_packageidupdate);
 		editText1.setText(packageId);
 
 		SensorCart.getValuesSetForSensor(sensorPosition);
+
 		TextView tempThreshold = (TextView) findViewById(R.id.text_temperatureThreshold);
-		tempThreshold.setText("Temperature Threshold: "
-				+ SensorCart.temperatureThreshold + " F");
 		TextView tempThresholdTime = (TextView) findViewById(R.id.text_temperatureThresholdTime);
-		tempThresholdTime
-				.setText("Notification Time:   Before- "
-						+ SensorCart.beforeTemperatureThreshold + " secs"
-						+ "   After- " + SensorCart.afterTemperatureThreshold
-						+ " secs");
+
+		if (("" + SensorCart.temperatureThreshold).contains("E")) {
+			tempThreshold.setText("Temperature is disabled");
+			tempThresholdTime.setText("Notification Time:   Before- " + "Never"
+					+ "   After- " + "Never");
+		} else {
+			tempThreshold.setText("Temperature Threshold: "
+					+ SensorCart.temperatureThreshold + " F");
+			tempThresholdTime.setText("Notification Time:   Before- "
+					+ SensorCart.beforeTemperatureThreshold + " secs"
+					+ "   After- " + SensorCart.afterTemperatureThreshold
+					+ " secs");
+		}
 
 		TextView humidityThreshold = (TextView) findViewById(R.id.text_humidityThreshold);
-		humidityThreshold.setText("Humidity Threshold: "
-				+ SensorCart.humidityThreshold + " %");
 		TextView humidityThresholdTime = (TextView) findViewById(R.id.text_humidityThresholdTime);
-		humidityThresholdTime.setText("Notification Time:   Before- "
-				+ SensorCart.beforeHumidityThreshold + " secs" + "   After- "
-				+ SensorCart.afterHumidityThreshold + " secs");
+
+		if (("" + SensorCart.humidityThreshold).contains("E")) {
+			humidityThreshold.setText("Humidity is disabled");
+			humidityThresholdTime.setText("Notification Time:   Before- "
+					+ "Never" + "   After- " + "Never");
+		} else {
+			humidityThreshold.setText("Humidity Threshold: "
+					+ SensorCart.humidityThreshold + " %");
+
+			humidityThresholdTime.setText("Notification Time:   Before- "
+					+ SensorCart.beforeHumidityThreshold + " secs"
+					+ "   After- " + SensorCart.afterHumidityThreshold
+					+ " secs");
+		}
 
 		TextView vibrationThreshold = (TextView) findViewById(R.id.text_vibrationThreshold);
-		vibrationThreshold.setText("Vibration Threshold: "
-				+ SensorCart.vibrationThreshold + " G");
 		TextView vibrationThresholdTime = (TextView) findViewById(R.id.text_vibrationThresholdTime);
-		vibrationThresholdTime.setText("Notification Time:   Before- "
-				+ SensorCart.beforeVibrationThreshold + " secs" + "   After- "
-				+ SensorCart.afterVibrationThreshold + " secs");
+
+		if (("" + SensorCart.vibrationThreshold).contains("E")) {
+			vibrationThreshold.setText("Vibration is disabled");
+			vibrationThresholdTime.setText("Notification Time:   Before- "
+					+ "Never" + "   After- " + "Never");
+		} else {
+			vibrationThreshold.setText("Vibration Threshold: "
+					+ SensorCart.vibrationThreshold + " G");
+			vibrationThresholdTime.setText("Notification Time:   Before- "
+					+ SensorCart.beforeVibrationThreshold + " secs"
+					+ "   After- " + SensorCart.afterVibrationThreshold
+					+ " secs");
+		}
 
 		TextView shockThreshold = (TextView) findViewById(R.id.text_shockThreshold);
-		shockThreshold.setText("Shock Threshold: " + SensorCart.shockThreshold
-				+ " G");
+		if (("" + SensorCart.shockThreshold).contains("E")) {
+			shockThreshold.setText("Shock is disabled");
+		} else {
+			shockThreshold.setText("Shock Threshold: "
+					+ SensorCart.shockThreshold + " G");
+		}
 
 		// get action bar
 		ActionBar actionBar = getActionBar();
@@ -83,7 +113,7 @@ public class EditSensorDetails extends Activity {
 			Toast.makeText(this, "Invalid Package Id", Toast.LENGTH_LONG)
 					.show();
 		} else {
-			listOfSensors.updatePackageId(sensorPosition,packageId);
+			listOfSensors.updatePackageId(sensorPosition, packageId);
 			String data = "updated";
 			Intent intent1 = new Intent();
 			intent1.putExtra(EXTRA_MESSAGE1, data);
