46a4c687e5eefba8b2693b7298f42243d3404c59
Thirumalai Samy Rajkumar <thrmlsam@gmail.com>
2014-01-28 20:23:06 -0800

    Include calibrate tab

--- code/SensorConfigurator/src/sensorconfiguration/swing/ui/UIEventHandler.java
+++ code/SensorConfigurator/src/sensorconfiguration/swing/ui/UIEventHandler.java
@@ -738,7 +738,7 @@ public class UIEventHandler extends ValidateUI implements Runnable, SensorEventH
             log(logText);
             UIObject.handleError(logText);
         }
-        bar.done();
+        
     }
 
 }
